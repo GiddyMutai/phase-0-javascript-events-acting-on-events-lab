eae03a568f66db5416c36041f42885d1f99e8bde
Gideon Mutai <giddymutai13@gmail.com>
2024-04-09 20:44:23 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+// grabs the dodger
+const dodger = document.getElementById("dodger");
+
+// function to move the dodger left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// function to move the dodger right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+// event listener
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else {
+    moveDodgerRight();
+  }
+});
